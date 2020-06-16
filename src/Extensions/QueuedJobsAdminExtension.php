7d01faef90c7c993e0fc455169cb937bb3c590b5
Aaron Carlino <unclecheese@leftandmain.com>
2020-06-16 16:27:10 +1200

    Remove jobs from queued jobs admin that don't belong

---
+++ src/Extensions/QueuedJobsAdminExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace SilverStripe\SearchService\Extensions;
+
+
+use SilverStripe\Core\Extension;
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\Form;
+use SilverStripe\SearchService\Jobs\IndexJob;
+use SilverStripe\SearchService\Jobs\RemoveDataObjectJob;
+
+class QueuedJobsAdminExtension extends Extension
+{
+    /**
+     * Remove jobs from the list that don't make sense to create from the admin (and won't work)
+     *
+     * @param Form $form
+     */
+    public function updateEditForm(Form $form)
+    {
+        $field = $form->Fields()->dataFieldByName('JobType');
+        /* @var DropdownField $field */
+        if ($field) {
+            $source = $field->getSource();
+            unset($source[IndexJob::class]);
+            unset($source[RemoveDataObjectJob::class]);
+            $field->setSource($source);
+        }
+    }
+}
