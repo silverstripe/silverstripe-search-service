225a203a59a0a22f21ae7e4186bf69358756ed0e
Chris Penny <cpenny@silverstripe.com>
2022-09-08 12:09:19 +1200

    Fetch LIVE DataObject when checking canView()

--- src/DataObject/DataObjectDocument.php
+++ src/DataObject/DataObjectDocument.php
@@ -121,8 +121,19 @@ class DataObjectDocument implements
         $dataObject = $this->getDataObject();
 
         // If an anonymous user can't view it
-        $isPublic = Member::actAs(null, function () use ($dataObject) {
-            return $dataObject->canView();
+        $isPublic = Member::actAs(null, static function () use ($dataObject) {
+            // Need to make sure that the version of the DataObject that we access is always the LIVE version
+            return Versioned::withVersionedMode(static function () use ($dataObject): bool {
+                Versioned::set_stage(Versioned::LIVE);
+
+                $liveDataObject = DataObject::get($dataObject->ClassName)->byID($dataObject->ID);
+
+                if (!$liveDataObject || !$liveDataObject->exists()) {
+                    return false;
+                }
+
+                return $liveDataObject->canView();
+            });
         });
 
         if (!$isPublic) {
